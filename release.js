7ba9f606412be3145bf7b738f761fbe8ca01fca2
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-07-12 13:29:49 +0100

    Don't use child_process.execSync because it's unavailable in Node.js 0.10

--- release.js
+++ release.js
@@ -6,18 +6,24 @@ var fs = require('fs-extra');
 fs.removeSync('dist');
 fs.mkdirSync('dist');
 
-childProcess.execSync('cd ../clouseau && python -m clouseau.stability.crashes -o "' + path.join(process.cwd(), 'dist') + '"');
-
-[
-  'index.html', 'dash.js', 'style.css', 'exclamation_mark.svg',
-  'question_mark.svg', 'rocket_fly.png',
-].forEach(function(file) {
-  fs.copySync(file, path.join('dist', file));
-});
-
-ghpages.publish('dist', function(err) {
-  if (err) {
-    console.error('Error while publishing to gh-pages');
-    console.error(err);
+childProcess.exec('cd ../clouseau && python -m clouseau.stability.crashes -o "' + path.join(process.cwd(), 'dist') + '"', function (error, stdout, stderr) {
+  if (error) {
+    console.error(stdout);
+    console.error(stderr);
+    return;
   }
+
+  [
+    'index.html', 'dash.js', 'style.css', 'exclamation_mark.svg',
+    'question_mark.svg', 'rocket_fly.png',
+  ].forEach(function(file) {
+    fs.copySync(file, path.join('dist', file));
+  });
+
+  ghpages.publish('dist', function(err) {
+    if (err) {
+      console.error('Error while publishing to gh-pages');
+      console.error(err);
+    }
+  });
 });
