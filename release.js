77009e3b3b8e3f6b1d0e951976b63195bf472bd7
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-07-12 13:05:12 +0100

    Make release.js automatically regenerate the data files using clouseau

--- release.js
+++ release.js
@@ -1,13 +1,16 @@
 var ghpages = require('gh-pages');
 var path = require('path');
+var childProcess = require('child_process');
 var fs = require('fs-extra');
 
 fs.removeSync('dist');
 fs.mkdirSync('dist');
+
+childProcess.execSync('cd ../clouseau && python -m clouseau.stability.crashes -o "' + path.join(process.cwd(), 'dist') + '"');
+
 [
-  'index.html', 'dash.js', 'style.css',
-  'exclamation_mark.svg', 'question_mark.svg', 'rocket_fly.png',
-  'release.json', 'release-startup.json', 'beta.json', 'beta-startup.json', 'aurora.json', 'aurora-startup.json',
+  'index.html', 'dash.js', 'style.css', 'exclamation_mark.svg',
+  'question_mark.svg', 'rocket_fly.png',
 ].forEach(function(file) {
   fs.copySync(file, path.join('dist', file));
 });
